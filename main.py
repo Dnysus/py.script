87e5f995c2b5869ade1d9c2261d411d1d1694bf8
Dion <54122936+Dnysus@users.noreply.github.com>
2022-02-03 16:12:11 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-df_csv = pd.read_csv(r"C:\Users\Dion\Desktop\Serial.csv")
+df_csv = pd.read_csv(r"path of file.csv")
 
 pd.set_option('display.max_rows', df_csv.shape[0])
 pd.set_option('display.max_columns', None)
 
-dups = df_csv.pivot_table(index = ['Serial Number'], aggfunc = 'size')
+dups = df_csv.pivot_table(index = ['name of what you want to count duplicates'], aggfunc = 'size')
 
 
 print(dups)
 
 
-dups.to_csv(r"C:\Users\Dion\Desktop\OnlyDups.csv", index = True)
+dups.to_csv(r"path where you want to save the generated file\name.csv", index = True)
